88091b496ecb3a4bcb8d58961971e2a6393d2465
Ariel-Reyes <73078760+Ariel-Reyes@users.noreply.github.com>
2021-04-23 13:53:45 -0600

    Agregacion del segundo ejercicio

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
 
 	int opcion=0;
 	while(opcion!=4) {
+		cout<<endl; 
 		cout<<".........Menu............."<<endl;
 		cout<<"1....................Tablero "<<endl;
 		cout<<"2....................Sumatoria"<<endl;
@@ -19,80 +20,65 @@ int main() {
 				cout<<"Ingrese el valor de N: "<<endl;
 				cin>>n;
 
+				int i =0;
+				while(i<n) {
 
-				/*for(int i=0; i<n; i++) {
-					
-                     if(i%2==0){
-                     		for(int j=0; j<n; j++) {
-						if(j%2==0) {
-							cout<<"0";
-						} else {
-							cout<<"1";
-
-						}
-					}
-					cout<<endl; 
-					
-					 } else{
-					 			for(int j=0; j<n; j++) {
-						if(j%2==0) {
-							cout<<"1";
-						} else {
-							cout<<"0";
-
-						}
-					}
-					cout<<endl; 
-					 }
-				
-				} */
-				int i =0; 
-				while(i<n){
-					
-					if(i%2==0){
-						int j=0; 
-						while(j<n){
-							if(j%2==0){
-								cout<<"0"; 
-							}else{
-								cout<<"1"; 
+					if(i%2==0) {
+						int j=0;
+						while(j<n) {
+							if(j%2==0) {
+								cout<<"0";
+							} else {
+								cout<<"1";
 							}
 							j++;
 						}
-						cout<<endl; 
-						
+						cout<<endl;
+
 					} else {
-						int z=0; 
-						while(z<n){
-							if(z%2==0){
-								cout<<"1"; 
-							}else{
-								cout<<"0"; 
+						int z=0;
+						while(z<n) {
+							if(z%2==0) {
+								cout<<"1";
+							} else {
+								cout<<"0";
 							}
 							z++;
 						}
-						cout<<endl; 
+						cout<<endl;
 					}
-					
-					
-					i++; 
-					
+
+
+					i++;
+
 				}
-				
-				
-
-
-
-
 				break;
 			}
 
 			case 2: {
-
+				int n;
+                  cout<<"Ingrese el valor de N: "<<endl;
+                  cin>>n; 
+                  int i=1;
+                  double acu =0; 
+                  while(i<=n){
+                  	double formula = (2*i) * (i-1);
+                  	acu = acu + formula; 
+                  	i++;
+				  }
+				  cout<<"El resultado es: "<<acu; 
+                  
+                  
+                  
 				break;
 			}
 
 			case 3: {
+				int n,t; 
+				cout<<"Ingrese el valor de N: "<<endl;
+				cin>>n; 
+				cout<<"Ingrese el valor de T: "<<endl; 
+				cin>>t; 
 
 				break;
 			}
